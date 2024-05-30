bb808bd358d3c8c0c24f48f0774a1b7da5a7ce18
Giuseppe Leonardi <giuseppe.leonardi@itiscuneo.eu>
2024-05-30 12:55:58 +0200

    Last minute fix

--- start.py
+++ start.py
@@ -4,21 +4,10 @@ import webbrowser
 
 
 class SimpleHTTP404RequestHandler(http.server.SimpleHTTPRequestHandler):
-    """
-    Overrides the default request handler to handle custom 404 pages as 404.html
-    (i.e. a 404.html page located in the root). This behavior is seen on:
-        GitHub:     https://help.github.com/articles/custom-404-pages/
-        FastMail:   https://www.fastmail.com/help/files/website.html
-
-    """
     def do_GET(self):
         path = self.translate_path(self.path)
-
-        # If the path doesn't exist, fake it to be the 404 page.
         if not os.path.exists(path):
             self.path = '404.html'
-
-        # Call the superclass methods to actually serve the page.
         http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 
